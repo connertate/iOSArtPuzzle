c6f52504420c8805dea58c13ab2a4ea9b407c815
Conner Tate <connertate8@gmail.com>
2022-12-02 22:10:37 -0500

    Initial Commit

---
+++ Art Puzzle/Art_PuzzleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Art_PuzzleApp.swift
+//  Art Puzzle
+//
+//  Created by Conner Tate on 12/2/22.
+//
+
+import SwiftUI
+
+@main
+struct Art_PuzzleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
